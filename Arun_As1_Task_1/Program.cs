b53d6e6a95880809c3d1c0cba8aa056543085488
arun7 <arun7@DESKTOP-UE0E1P0>
2025-01-29 16:42:54 -0500

    Add project files.

---
+++ Arun_As1_Task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Arun_As1_Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
